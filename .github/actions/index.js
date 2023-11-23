f4b4799bc1d6198f9c008a9a703e46572db09bb0
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2023-11-23 10:48:24 +0100

    Trying to show issues

--- .github/actions/index.js
+++ .github/actions/index.js
@@ -10,10 +10,16 @@ const instruments = JSON.parse(fs.readFileSync('instruments.json', 'utf-8'));
 const tasks = JSON.parse(fs.readFileSync('tasks.json', 'utf-8'));
 
 async function getIssues() {
-  await octokit.paginate(octokit.rest.issues.listForRepo, {
+  octokit.paginate(octokit.rest.issues.listForRepo, {
     owner: process.env.GITHUB_REPOSITORY_OWNER,
     repo: process.env.GITHUB_REPO_NAME,
-    });
+    })
+      .then(issues => {
+        issues.forEach(x => {
+          console.log(`Issue ${x.number} with body ${x.body} and last opened ?`);
+          console.log(JSON.stringify(x));
+        })
+      })
 }
 
 async function runAction() {
