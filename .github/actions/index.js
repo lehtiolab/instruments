9b12c3903e483937fe37544a7fd92ee63add5732
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2023-11-23 19:42:44 +0100

    testing octokit issues

--- .github/actions/index.js
+++ .github/actions/index.js
@@ -7,9 +7,11 @@ const token = core.getInput('repo-token');
 const octokit = github.getOctokit(token);
 
 const instruments = JSON.parse(fs.readFileSync('instruments.json', 'utf-8'));
-const tasks = JSON.parse(fs.readFileSync('tasks.json', 'utf-8'));
+const tasklist = JSON.parse(fs.readFileSync('tasks.json', 'utf-8'));
+const tasks = Object.fromEntries(tasklist.map(x => [x.name, x]));
 
-async function getIssues() {
+function getIssues() {
+  let issues = {};
   octokit.paginate(octokit.rest.issues.listForRepo, {
     owner: process.env.GITHUB_REPOSITORY_OWNER,
     repo: process.env.GITHUB_REPO_NAME,
@@ -20,14 +22,51 @@ async function getIssues() {
           console.log(JSON.stringify(x));
         })
       })
+  return issues;
 }
 
-async function runAction() {
-  const issues = await getIssues();
+
+function checkEditedInstrumentsOrTasks() {
+  const issues = getIssues();
+    // for each instrument, check if an issue exists for each task
+    // // remove issues for that instrument if task is not in instrument list
+    // for each issue, if no instrument, remove it
+    //
+    // for each task, check if an orphan issue exists and remove error label
+    // // for each issue, check if it needs error labeling
+    //
 }
 
-runAction();
+function editIssuesOrderByDate() {
+    // get all issues, and edit them in order of due date
+    // this way one can sort them by last edited on GH
+}
 
+
+function reopenIssueAndSetDueDate(issuenumber) {
+  const issue = octokit.rest.issues.get({
+    owner: process.env.GITHUB_REPOSITORY_OWNER,
+    repo: process.env.GITHUB_REPO_NAME,
+    issue_number: issuenumber,
+  });
+  const issuedata = fm(issue.body);
+  console.log(JSON.stringify(issuedata));
+
+  //octokit.rest.issues.update({
+  //  owner: process.env.GITHUB_REPOSITORY_OWNER,
+  //  repo: process.env.GITHUB_REPO_NAME,
+  //  issue_number: issuenumber,
+  //  body: "",
+  //  title: "",
+  //})
+
+  editIssuesOrderByDate();
+}
+
+// runAction();
+reopenIssueAndSetDueDate(1);
+
+// if /case switch for commands
 console.log(instruments);
 console.log(tasks);
 
